f0d65705a95368ab3c5cf0168904a33090c51422
Michael Hicks <hicks.michael04@gmail.com>
2022-12-05 21:07:00 -0700

    modify main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.tracer(0)
+screen.tracer(0)  # turns off tracer
 
 game_is_on = True
 while game_is_on:
